917f70afc2e968af5a8a9c984d847cec02aa900e
hyunho <mizzking75@gmail.com>
2022-05-22 09:54:22 +0900

    Initial Commit

---
+++ LullabyRecipe/LullabyRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LullabyRecipeApp.swift
+//  LullabyRecipe
+//
+//  Created by hyunho lee on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LullabyRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
