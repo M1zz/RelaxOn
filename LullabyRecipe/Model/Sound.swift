6837b9b5e811a6aa1ecc326b918d20f5679ef257
ccdkk <91456952+ccdkk@users.noreply.github.com>
2022-06-25 16:46:43 +0900

    [refactor] DummyData파일 Model 폴더에 추가

--- LullabyRecipe/Model/Sound.swift
+++ LullabyRecipe/Model/Sound.swift
@@ -106,29 +106,3 @@ var naturalSound: Sound = Sound(id: 6,
 
 var mixedAudioSources: [Sound] = []
 var userRepositories: [MixedSound] = []
-
-let dummyMixedSound = MixedSound(id: 0,
-                                 name: "test",
-                                 baseSound: dummyBaseSound,
-                                 melodySound: dummyMelodySound,
-                                 naturalSound: dummyNaturalSound,
-                                 imageName: "Recipe1")
-
-let dummyBaseSound = Sound(id: 0,
-                           name: BaseAudioName.chineseGong.fileName,
-                           soundType: .base,
-                           audioVolume: 0.8,
-                           imageName: "gong1")
-
-let dummyMelodySound = Sound(id: 2,
-                             name: MelodyAudioName.lynx.fileName,
-                             soundType: .melody,
-                             audioVolume: 1.0,
-                             imageName: "Melody1")
-
-let dummyNaturalSound = Sound(id: 6,
-                              name: NaturalAudioName.creekBabbling.fileName,
-                              soundType: .natural,
-                              audioVolume: 0.4,
-                              imageName: "field")
-
