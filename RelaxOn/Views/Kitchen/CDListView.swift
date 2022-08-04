689487a42719ee726f4b3ff58fa674ddad12d3e0
limhyoseok <nonamedoud@gmail.com>
2022-08-04 22:16:56 +0900

    [feat] ui 구현 완료

--- RelaxOn/Views/Kitchen/CDListView.swift
+++ RelaxOn/Views/Kitchen/CDListView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+
+
 struct CDListView: View {
     @State var userRepositoriesState: [MixedSound] = userRepositories
     @State var selectedImageNames = (
@@ -15,23 +17,43 @@ struct CDListView: View {
         natural: ""
     )
     @State var isEditMode = false
-    @State var selectedMixedSoundId: [Int] = []
+    @State var selectedMixedSoundIds: [Int] = []
     
     var body: some View {
         
         VStack {
             libraryHeader
-            
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVGrid(columns: Array(repeating: GridItem(.flexible(),spacing: 10), count: 2), spacing: 20) {
                     plusCDImage
                     ForEach(userRepositoriesState.reversed()){ mixedSound in
                         CDCardView(data: mixedSound, audioVolumes: (baseVolume: mixedSound.baseSound?.audioVolume ?? 1.0, melodyVolume: mixedSound.melodySound?.audioVolume ?? 1.0, naturalVolume: mixedSound.naturalSound?.audioVolume ?? 1.0))
                             .disabled(isEditMode)
-                            .onTapGesture {
-                                print("\(mixedSound.id)")
+                            .overlay(alignment : .bottomTrailing) {
+                                if isEditMode {
+                                    if selectedMixedSoundIds.firstIndex(where: {$0 == mixedSound.id}) != nil {
+                                        Image(systemName: "checkmark.circle.fill")
+                                            .foregroundColor(.white)
+                                            .padding(.bottom, LayoutConstants.pading.bottomOfRadioButton)
+                                            .padding(.trailing, LayoutConstants.pading.trailingOfRadioButton)
+                                    } else {
+                                        Image(systemName: "circle")
+                                            .foregroundColor(.white)
+                                            .background(Image(systemName: "circle.fill").foregroundColor(.gray).opacity(0.5))
+                                            .padding(.bottom, LayoutConstants.pading.bottomOfRadioButton)
+                                            .padding(.trailing, LayoutConstants.pading.trailingOfRadioButton)
+                                    }
+                                }
                             }
-    
+                            .onTapGesture {
+                                if isEditMode {
+                                    if let index = selectedMixedSoundIds.firstIndex(where: {$0 == mixedSound.id}) {
+                                        selectedMixedSoundIds.remove(at: index)
+                                    } else {
+                                        selectedMixedSoundIds.append(mixedSound.id)
+                                    }
+                                }
+                            }
                     }
                 }
             }
@@ -74,9 +96,14 @@ struct CDListView: View {
             Spacer()
             
             Button(action: {
-                isEditMode.toggle()
+                if selectedMixedSoundIds.isEmpty {
+                    isEditMode.toggle()
+                } else {
+                    print("hello")
+                    print(selectedMixedSoundIds)
+                }
             }) {
-                if selectedMixedSoundId.isEmpty {
+                if selectedMixedSoundIds.isEmpty {
                     Text(isEditMode ? "Done" : "Edit")
                         .foregroundColor(Color.gray)
                         .font(.system(size: 17))
@@ -106,8 +133,57 @@ struct CDListView: View {
     }
 }
 
+extension CDListView {
+    private struct LayoutConstants {
+        enum pading {
+            static let trailingOfRadioButton: CGFloat = 10
+            static let bottomOfRadioButton: CGFloat = 40
+        }
+    }
+}
+
 struct CDListView_Previews: PreviewProvider {
+    struct dummy {
+        static let MixedSound1 = MixedSound(id: 0,
+                                         name: "test",
+                                         baseSound: dummyBaseSound,
+                                         melodySound: dummyMelodySound,
+                                         naturalSound: dummyNaturalSound,
+                                         imageName: "Recipe1")
+        
+        static let MixedSound2 = MixedSound(id: 1,
+                                         name: "test2",
+                                         baseSound: dummyBaseSound,
+                                         melodySound: dummyMelodySound,
+                                         naturalSound: dummyNaturalSound,
+                                         imageName: "Recipe2")
+        
+        static let MixedSound3 = MixedSound(id: 2,
+                                         name: "test3",
+                                         baseSound: dummyBaseSound,
+                                         melodySound: dummyMelodySound,
+                                         naturalSound: dummyNaturalSound,
+                                         imageName: "Recipe3")
+        
+        static let dummyBaseSound = Sound(id: 0,
+                                   name: BaseAudioName.longSun.fileName,
+                                   soundType: .base,
+                                   audioVolume: 0.8,
+                                   imageName: "LongSun")
+
+        static let dummyMelodySound = Sound(id: 2,
+                                     name: MelodyAudioName.ambient.fileName,
+                                     soundType: .melody,
+                                     audioVolume: 1.0,
+                                     imageName: "Melody1")
+
+        static let dummyNaturalSound = Sound(id: 6,
+                                      name: NaturalAudioName.dryGrass.fileName,
+                                      soundType: .natural,
+                                      audioVolume: 0.4,
+                                      imageName: "field")
+    }
     static var previews: some View {
-        CDListView()
+        CDListView(userRepositoriesState: [dummy.MixedSound1, dummy.MixedSound2, dummy.MixedSound3])
     }
 }
