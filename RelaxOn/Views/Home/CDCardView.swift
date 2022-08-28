426cc3762060a288152802897ee43f03052efef8
ccdkk <cdk243@gmail.com>
2022-08-28 10:16:00 +0900

    [fix] #197 emptySound의 image에 투명도 0 주기

--- RelaxOn/Views/Home/CDCardView.swift
+++ RelaxOn/Views/Home/CDCardView.swift
@@ -20,18 +20,24 @@ struct CDCardView: View {
                 self.isShwoingMusicView.toggle()
             }, label: {
                     ZStack {
-                        Image(data.baseSound?.imageName ?? "")
-                            .resizable()
-                            .opacity(0.5)
-                            .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
-                        Image(data.melodySound?.imageName ?? "")
-                            .resizable()
-                            .opacity(0.5)
-                            .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
-                        Image(data.whiteNoiseSound?.imageName ?? "")
-                            .resizable()
-                            .opacity(0.5)
-                            .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
+                        if let baseSoundImageName = data.baseSound?.imageName {
+                            Image(baseSoundImageName)
+                                .resizable()
+                                .opacity(baseSoundImageName == "music" ? 0 : 0.5)
+                                .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
+                        }
+                        if let melodySoundImageName = data.melodySound?.imageName {
+                            Image(melodySoundImageName)
+                                .resizable()
+                                .opacity(melodySoundImageName == "music" ? 0 : 0.5)
+                                .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
+                        }
+                        if let whiteNoiseSoundImageName = data.whiteNoiseSound?.imageName {
+                            Image(whiteNoiseSoundImageName)
+                                .resizable()
+                                .opacity(whiteNoiseSoundImageName == "music" ? 0 : 0.5)
+                                .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.width * 0.43)
+                        }
                     }
                     .fullScreenCover(item: $selectedMixedSound) { _ in
                         NewMusicView(data: data, userRepositoriesState: $userRepositoriesState)
