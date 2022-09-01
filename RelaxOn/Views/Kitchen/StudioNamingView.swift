dfdc219b76114b1df80b5fd1ba6a34148100a78a
Hardydev <dusgh15378@gmail.com>
2022-09-01 23:17:53 +0900

    [fix] : StudioNamingView 수정

--- RelaxOn/Views/Kitchen/StudioNamingView.swift
+++ RelaxOn/Views/Kitchen/StudioNamingView.swift
@@ -26,25 +26,25 @@ struct StudioNamingView: View {
             VStack {
                 HStack {
                     NamingBackButton()
-                        .padding(.horizontal, 15)
+                        .padding(.horizontal)
                     Spacer()
                 }
 
                 HStack {
                     Text("Please name this CD")
-                        .frame(width: deviceFrame.exceptPaddingWidth / 2)
                         .font(.system(size: 28, weight: .medium))
                         .foregroundColor(.white)
-                        .lineLimit(2)
                     Spacer()
-                }.padding()
+                }
+                .padding(.horizontal)
+                .padding(.top, deviceFrame.screenHeight * 0.04)
 
                 VStack {
                     TextField("", text: $textEntered)
                         .foregroundColor(.white)
                         .modifier(PlaceholderCustom(showPlaceHolder: textEntered.isEmpty, placeHolder: "Make your own CD"))
                         .keyboardType(.alphabet)
-                        .padding(.horizontal, 15)
+                        .padding(.horizontal)
 
                     Rectangle()
                         .foregroundColor(.white)
