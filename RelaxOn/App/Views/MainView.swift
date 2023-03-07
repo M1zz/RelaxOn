53160c9234a65f3c3840bd6c9135c0252a9ee88e
doyeonjeong <debby_@kakao.com>
2023-03-07 23:07:02 +0900

    🎉 :: 기존 코드 레거시 처리 & 버전 15.0->16.0으로 변경

---
+++ RelaxOn/App/Views/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/03/07.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
