76a523c704ad793bc458c68663710de184146d60
doyeonjeong <debby_@kakao.com>
2023-03-10 23:42:48 +0900

    🎨[Design]: SoundListView 레이아웃 조정

--- RelaxOn/App/Views/Create/SoundListView.swift
+++ RelaxOn/App/Views/Create/SoundListView.swift
@@ -7,17 +7,49 @@
 
 import SwiftUI
 
-struct FileInfo: Hashable {
-    let name: String
-}
-
 struct SoundListView: View {
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    let fileNames: [String] = ["Water Drop", "Gong", "Twitter", "Wind", "Wave1", "Wave2"]
+    
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: SoundDetailView(fileName: "첫번째")) {
-                Text("SoundSelectView")
+            ScrollView {
+                gridView()
             }
         }
+    }
+    
+    @ViewBuilder
+    private func gridView() -> some View {
+        LazyVGrid(columns: columns) {
+            ForEach(fileNames, id: \.self) { fileName in
+                NavigationLink(destination: SoundDetailView(fileName: "파일 이름 : \(fileName)")) {
+                    gridViewItem(fileName)
+                }
+            }
+        }
+        .padding(20)
+        .padding(.top, 10)
+    }
+    
+    @ViewBuilder
+    private func gridViewItem(_ fileName: String) -> some View {
+        VStack(alignment: .leading) {
+            Text(fileName)
+                .font(.title2)
+                .bold()
+            Image(systemName: "photo")
+                .frame(width: 160, height: 160)
+                .background {
+                    Color.systemGrey1
+                }
+        }
+        .foregroundColor(.black)
     }
 }
 
