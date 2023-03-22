c71317fe3409abd7fff0f287f92705a7ff06216f
HamSik <payback9488@gmail.com>
2023-03-22 15:27:11 +0900

    Changes to be committed   🎨[Design]: Cancel Button UI 생성
    	Cancel Button UI 생성했습니다!

--- RelaxOn/App/Views/Create/SoundSaveView.swift
+++ RelaxOn/App/Views/Create/SoundSaveView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct SoundSaveView: View {
-
+    
     @Binding var volume: Float
     
     var body: some View {
-        Text("\(volume)")
+        VStack{
+            HStack{
+                //취소 버튼
+                Button {
+                    print("cancel")
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding(10)
+                }.offset(x: 0, y: -70)
+            }
+        }
     }
 }
 
-//struct ThirdView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SoundSaveView(volume: @Binding변수)
-//    }
-//}
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundSaveView(volume: .constant(Float()))
+    }
+}
