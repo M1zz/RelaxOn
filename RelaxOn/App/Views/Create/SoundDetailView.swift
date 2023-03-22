d35af73608666d3e4bde19259dcfe563a4576168
HamSik <payback9488@gmail.com>
2023-03-22 13:33:07 +0900

    🎨[Design]: Sheet뷰에서 FullScreenCover로 변경 .sheet에서 .fullScreenCover로 변경하였습니다

--- RelaxOn/App/Views/Create/SoundDetailView.swift
+++ RelaxOn/App/Views/Create/SoundDetailView.swift
@@ -76,7 +76,7 @@ struct SoundDetailView: View {
                     .bold()
                     .font(.system(size: 20))
             }
-            .sheet(isPresented: $isShowingSheet) {
+            .fullScreenCover(isPresented: $isShowingSheet) {
                 SoundSaveView(volume: $volume)
             }
         }
