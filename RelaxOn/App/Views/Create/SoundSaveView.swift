411697eaca6cd96cf969257d905aa7e41aaa56f1
HamSik <payback9488@gmail.com>
2023-03-24 23:44:29 +0900

    ✨[Feat] Keyboard Dismiss
    	Keyboard의 Return버튼과 화면을 누르면
    	Keyboard가 꺼집니다!#

--- RelaxOn/App/Views/Create/SoundSaveView.swift
+++ RelaxOn/App/Views/Create/SoundSaveView.swift
@@ -14,70 +14,77 @@ struct SoundSaveView: View {
     @State var soundSavedName: String = ""
     @State var randomImageFileNumber = 1
     var imageFiles: [String] = ["Recipe1", "Recipe2", "Recipe3", "Recipe4", "Recipe5",
-                                 "Recipe6", "Recipe7", "Recipe8", "Recipe9", "Recipe10"]
+                                "Recipe6", "Recipe7", "Recipe8", "Recipe9", "Recipe10"]
     
     var body: some View {
-        VStack{
-            HStack{
-                
-                //취소 버튼
-                Button {
-                    print("cancel")
-                } label: {
-                    Text("Cancel")
-                        .foregroundColor(.black)
-                        .font(.system(size: 20))
-                        .bold()
-                        .padding(10)
-                }.offset(x: 0, y: -70)
-                
-                Spacer()
-                
-                //저장 버튼
-                Button {
-                    print("save")
-                    isFocused = false
-                } label: {
-                    Text("Save")
-                        .foregroundColor(.black)
-                        .font(.system(size: 20))
-                        .bold()
-                        .padding(10)
-                }.offset(x: 0, y: -70)
-            }
-            
-            //제목 입력
-            TextField("\(volume)", text: $soundSavedName)
-                .frame(minWidth: 150, idealWidth: 150, maxWidth: 300,
-                       minHeight: 80, idealHeight: 80, maxHeight: 80,
-                       alignment: .center)
-                .padding(EdgeInsets(top: 100, leading: 150, bottom: 100, trailing: 150))
-                .keyboardType(.default)
-                .autocorrectionDisabled(true)
-                .focused($isFocused)
-                .font(.title)
-                .underline(true)
-            ZStack{
-                
-                //앨범 이미지
-                Image(imageFiles[randomImageFileNumber])
-                    .resizable()
-                    .frame(width: 300, height: 300)
-                
-                //이미지변경 버튼
-                Button {
-                    randomImageFileNumber = Int(getRandomAlbumImage(from: imageFiles))
-                } label: {
-                    Image(systemName: "arrow.triangle.2.circlepath")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(.black)
-                        .bold()
+        ZStack{
+            VStack{
+                HStack{
                     
-                }.offset(x: 120, y: -120)
+                    //취소 버튼
+                    Button {
+                        print("cancel")
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(.black)
+                            .font(.system(size: 20))
+                            .bold()
+                            .padding(10)
+                    }.offset(x: 0, y: -70)
+                    
+                    Spacer()
+                    
+                    //저장 버튼
+                    Button {
+                        print("save")
+                        isFocused = false
+                    } label: {
+                        Text("Save")
+                            .foregroundColor(.black)
+                            .font(.system(size: 20))
+                            .bold()
+                            .padding(10)
+                    }.offset(x: 0, y: -70)
+                }
+                
+                //제목 입력
+                TextField("\(volume)", text: $soundSavedName)
+                    .frame(minWidth: 150, idealWidth: 150, maxWidth: 300,
+                           minHeight: 80, idealHeight: 80, maxHeight: 80,
+                           alignment: .center)
+                    .padding(EdgeInsets(top: 100, leading: 180, bottom: 100, trailing: 150))
+                    .keyboardType(.default)
+                    .autocorrectionDisabled(true)
+                    .focused($isFocused)
+                    .font(.title)
+                    .underline(true)
+                
+                ZStack{
+                    //앨범 이미지
+                    Image(imageFiles[randomImageFileNumber])
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                    
+                    //이미지변경 버튼
+                    Button {
+                        randomImageFileNumber = Int(getRandomAlbumImage(from: imageFiles))
+                    } label: {
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.black)
+                            .bold()
+                        
+                    }.offset(x: 120, y: -120)
+                }
             }
-        }.onAppear {
-            isFocused = true
+            .onAppear {
+                isFocused = true
+            }
+        }
+        .background(Color.white)
+        .onTapGesture {
+            isFocused = false
         }
     }
 }
