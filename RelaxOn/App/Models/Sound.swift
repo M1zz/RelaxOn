79b2268b4543314dc1ef7cac4ddb702748099fdb
DOYEON <108422901+doyeonjeong@users.noreply.github.com>
2023-03-27 15:49:21 +0900

    ✨[Feat]: Play & Pause Sound Auto (#359)
    * 🎨[Design]: Delete Play & Pause Buttons
    
    재생 정지 버튼 불필요하여  삭제했습니다.
    
    * ♻️[Refactor]: Reset MixedSound
    
    - MixedSound 구조체를 최대한 간소화했습니다.
    - 가장 먼저 전달해야하는 파일명, 볼륨만 전달해보겠습니다.
    
    * ➕[Add]: Add AudioManager
    
    - MixedSound 타입의 객체를 재생시키는 AudioManager를 생성했습니다.
    - 간단한 재생 정지 기능이 있습니다.
    - numberOfLoops 속성은 오디오 파일이 무한 루프로 반복되는 횟수를 설정합니다.
    
    * ✏️[Comment]: Description about AudioManager
    
    - audioPlayer의 세부 설정 관련하여 주석 추가했습니다.
    
    * ✨[Feat]: Play & Pause Sound Auto
    
    - SoundDetailView.onAppear : 자동으로 배경음악이 재생됩니다.
    - SoundDetailView.onDisappear : 자동으로 배경음악이 멈춥니다.
    - 재생되는 배경음악은 반복됩니다.
    - 볼륨을 조절하면 updateVolume()으로 바인딩합니다.
    
    * 🛠️ [Chore]: Fix some preview functions
    
    - Preview 볼 수 있도록 수정
    
    * 🐛[Fix](SoundDetailView): Delete @State volume
    
    - AudioManager의 @Published volume 사용하는 방식으로 변경
    - 추후 @State mixedSound도 createMixedSound()로 변경하겠습니다
    
    * 🗑️ [Del]: Delelte 2 Code
    
    - AudioManager : import Foundation 삭제
    - SoundDetailView : 주석처리 된 volume 변수 삭제
    
    * 🐛[Fix]: Edit AudioManager Access modifier
    
    - Final로 변경
    
    * 🐛[Fix]: Edit MusicExtension rawValue
    
    - 필요없는 rawValue 값 제거

--- RelaxOn/App/Models/Sound.swift
+++ RelaxOn/App/Models/Sound.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-struct Sound: Codable {
+struct Sound {
     let name: String
-    var audioVolume: Float
-    let fileName: String
-    
-    init(name: String, audioVolume: Float, fileName: String) {
-        self.name = name
-        self.audioVolume = audioVolume
-        self.fileName = fileName
-    }
-    
-    static let empty: (Int) -> Sound = { id in
-        Sound(name: "Empty", audioVolume: 0.5, fileName: "")
-    }
 }
