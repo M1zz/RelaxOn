b89674872ae34459b6a7cbfe6141e75e9ad119f5
doyeonjeong <debby_@kakao.com>
2023-04-12 12:25:59 +0900

    🐛[Fix]: SoundSaveView 멈춤 현상 수정
    - TextField가 아닌 다른곳을 누르면 앱이 멈추는 현상 Fix
    - 필요없는 Zstack 삭제
    - .onTapGesture - isFocused = false가 아닌 키보드 숨김으로 수정

--- RelaxOn/App/Views/Create/SoundSaveView.swift
+++ RelaxOn/App/Views/Create/SoundSaveView.swift
@@ -15,77 +15,75 @@ struct SoundSaveView: View {
     @State private var alertMessage = ""
     @State var soundSavedName: String = ""
     @State var mixedSound: MixedSound
-
+    
     var body: some View {
-        ZStack{
-            VStack{
-                HStack{
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Text("Cancel")
-                            .foregroundColor(.black)
-                            .font(.system(size: 20))
-                            .bold()
-                            .padding(10)
-                    }.offset(x: 0, y: -70)
-                    
-                    Spacer()
-
-                    Button {
-                        print("save")
-                        isFocused = false
-                        let newMixedSound = MixedSound(name: soundSavedName, imageName: mixedSound.imageName)
-                        do {
-                            try UserFileManager.shared.saveMixedSound(newMixedSound)
-                            presentationMode.wrappedValue.dismiss() // 성공적으로 저장되면 dismiss
-                        } catch {
-                            print(error.localizedDescription)
-                            isShowingAlert = true
-                            alertMessage = "저장에 실패했습니다. \(error.localizedDescription)"
-                        }
-                    } label: {
-                        Text("Save")
-                            .foregroundColor(.black)
-                            .font(.system(size: 20))
-                            .bold()
-                            .padding(10)
-                    }.offset(x: 0, y: -70)
-                }
-
-                TextField(mixedSound.name, text: $soundSavedName)
-                    .frame(width: 300, height: 80, alignment: .center)
-                    .multilineTextAlignment(.center)
-                    .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
-                    .keyboardType(.default)
-                    .autocorrectionDisabled(true)
-                    .focused($isFocused)
-                    .font(.title)
-                    .underline(true)
+        VStack{
+            HStack{
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding(10)
+                }.offset(x: 0, y: -70)
                 
-                ZStack{
-                    Image(mixedSound.imageName)
+                Spacer()
+                
+                Button {
+                    print("save")
+                    isFocused = false
+                    let newMixedSound = MixedSound(name: soundSavedName, imageName: mixedSound.imageName)
+                    do {
+                        try UserFileManager.shared.saveMixedSound(newMixedSound)
+                        presentationMode.wrappedValue.dismiss() // 성공적으로 저장되면 dismiss
+                    } catch {
+                        print(error.localizedDescription)
+                        isShowingAlert = true
+                        alertMessage = "저장에 실패했습니다. \(error.localizedDescription)"
+                    }
+                } label: {
+                    Text("Save")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding(10)
+                }.offset(x: 0, y: -70)
+            }
+            
+            TextField(mixedSound.name, text: $soundSavedName)
+                .frame(width: 300, height: 80, alignment: .center)
+                .multilineTextAlignment(.center)
+                .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                .keyboardType(.default)
+                .autocorrectionDisabled(true)
+                .focused($isFocused)
+                .font(.title)
+                .underline(true)
+            
+            ZStack{
+                Image(mixedSound.imageName)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                Button {
+                    print("이미지 변경 버튼")
+                    mixedSound.imageName = recipeRandomName.randomElement()!
+                } label: {
+                    Image(systemName: "arrow.triangle.2.circlepath")
                         .resizable()
-                        .frame(width: 300, height: 300)
-                    Button {
-                        print("이미지 변경 버튼")
-                        mixedSound.imageName = recipeRandomName.randomElement()!
-                    } label: {
-                        Image(systemName: "arrow.triangle.2.circlepath")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .foregroundColor(.black)
-                            .bold()
-                    }.offset(x: 120, y: -120)
-                }
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.black)
+                        .bold()
+                }.offset(x: 120, y: -120)
             }
-            .onAppear {
-                isFocused = true
-            }
+        }
+        .onAppear {
+            isFocused = true
         }
         .background(Color.white)
         .onTapGesture {
-            isFocused = false
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
         .alert(isPresented: $isShowingAlert) {
             Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
