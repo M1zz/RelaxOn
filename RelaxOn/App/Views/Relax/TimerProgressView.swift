dbf7bbe8bed08416e64d5247d6a6df2e4266ed90
HamSik <payback9488@gmail.com>
2023-04-12 16:16:27 +0900

    ➕[Add]: TimerProgressView.swift
    	타이머 진행을 보여주는 뷰파일을 추가하였습니다!

---
+++ RelaxOn/App/Views/Relax/TimerProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerProgressView.swift
+//  RelaxOn
+//
+//  Created by 황석현 on 2023/04/12.
+//
+
+import SwiftUI
+
+struct TimerProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerProgressView()
+    }
+}
