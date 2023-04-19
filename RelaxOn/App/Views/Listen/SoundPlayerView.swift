3340e3122f93ff2f37b7af4ebd3771ccc35fc09a
HamSik <payback9488@gmail.com>
2023-04-19 10:01:15 +0900

    ➕[Add]: Add SoundPlayerView.swift
    	파일 추가합니다

---
+++ RelaxOn/App/Views/Listen/SoundPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  SoundPlayerView.swift
+//  RelaxOn
+//
+//  Created by 황석현 on 2023/04/19.
+//
+
+import SwiftUI
+
+struct SoundPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SoundPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundPlayerView()
+    }
+}
