2e47c92f9d4ab8c4c629b38116dc9701b91d142e
doyeonjeong <debby_@kakao.com>
2023-05-17 23:40:49 +0900

    ➕[Add]: Common, Enums 파일 생성
    - Utils 하위에 생성

---
+++ RelaxOn/App/Utils/Common.swift
@@ -0,0 +1,10 @@
+//
+//  Common.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/05/17.
+//
+
+/// Public 하게 쓰여지는 프로퍼티 & 함수 등
+
+import Foundation
---
+++ RelaxOn/App/Utils/Enums.swift
@@ -0,0 +1,10 @@
+//
+//  Enums.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/05/17.
+//
+
+/// Enum으로 관리되는 모든 것
+
+import Foundation
