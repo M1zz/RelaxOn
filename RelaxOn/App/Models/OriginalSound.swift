4882e499db54b108c84b7c75f8930d28da809f0e
doyeonjeong <debby_@kakao.com>
2023-05-18 16:28:00 +0900

    ➕[Add]: OriginalSound, CustomSound 객체 추가

--- RelaxOn/App/Models/OriginalSound.swift
+++ RelaxOn/App/Models/OriginalSound.swift
@@ -7,12 +7,10 @@
 
 import Foundation
 
-// TODO: OriginalSound로 이름 변경
-// TODO: 내부 구조 변경
-
 /**
  앱 번들에 있는 원본 사운드를 불러오기 위한 구조체
  */
+// TODO: OriginalSound로 교체 필요
 struct Sound {
     let name: String
     var volume: Float
@@ -21,6 +19,19 @@ struct Sound {
     init(name: String, volume: Float = 0.5, imageName: String = "placeholderImage") {
         self.name = name
         self.volume = volume
+        self.imageName = imageName
+    }
+}
+
+/**
+ 앱 번들에 있는 원본 사운드를 불러오기 위한 구조체
+ */
+struct OriginalSound {
+    let name: String
+    let imageName: String
+    
+    init(name: String, imageName: String = "placeholderImage") {
+        self.name = name
         self.imageName = imageName
     }
 }
