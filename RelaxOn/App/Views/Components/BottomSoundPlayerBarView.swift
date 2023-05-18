a3feda4d513da8100460896d0fb080594ed17a1b
doyeonjeong <debby_@kakao.com>
2023-05-18 15:13:55 +0900

    🐛[Fix]: 중복된 파일명 수정
    - Legacy 폴더에 있는 Extension 파일명 중복으로 인해 빌드 불가능 문제 해결

---
+++ RelaxOn/App/Views/Components/BottomSoundPlayerBarView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSoundPlayerBarView.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/05/18.
+//
+
+import SwiftUI
+
+struct BottomSoundPlayerBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomSoundPlayerBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSoundPlayerBarView()
+    }
+}
---
+++ RelaxOn/App/Views/Components/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/05/18.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}
