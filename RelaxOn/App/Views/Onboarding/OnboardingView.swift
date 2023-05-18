5b5ca22f9d0dcdba1fca5cf218d48994db9985b2
doyeonjeong <debby_@kakao.com>
2023-05-18 22:03:43 +0900

    ➕[Add]: OnboardingView, TutorialView 파일 추가 #390 #391

---
+++ RelaxOn/App/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  RelaxOn
+//
+//  Created by Doyeon on 2023/05/18.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
