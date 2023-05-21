4c9fa5c2bc3b8d1870d051fdd4eaffc01195b4d6
hamsik22 <payback9488@gmail.com>
2023-05-22 02:48:01 +0900

    🎨[Design]: Design OnboardingView
    	* OnboardingView 초기 디자인 완료

--- RelaxOn/App/Views/Onboarding/OnboardingView.swift
+++ RelaxOn/App/Views/Onboarding/OnboardingView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    var onboardingImages = ["1-dark", "2-dark", "3-dark", "4-dark"]
+    @State var currentStep = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .bottom) {
+            TabView(selection: $currentStep) {
+                ForEach(0..<4) { num in
+                    Image(onboardingImages[num])
+                        .resizable()
+                        .frame(maxWidth: .infinity)
+                        .ignoresSafeArea()
+                }
+            }.tabViewStyle(.page(indexDisplayMode: .never))
+            
+            Button {
+                if currentStep < onboardingImages.count  { self.currentStep += 1
+                    print(currentStep)
+                } else {
+                }
+            } label: {
+                Text(currentStep == onboardingImages.count ? "계속" : "시작하기")
+                    .padding(12)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.purple)
+                    .cornerRadius(15)
+                    .padding(30)
+                
+            }
+        }.ignoresSafeArea()
     }
 }
-
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
