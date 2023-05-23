629a495e7841800d94bd0af67ec5fc9c79d4fe57
doyeonjeong <debby_@kakao.com>
2023-05-23 13:30:49 +0900

    ➕[Add]: SoundListView에서 보여줄 OriginalSound 배열 객체 선언

--- RelaxOn/App/Utils/Common.swift
+++ RelaxOn/App/Utils/Common.swift
@@ -15,13 +15,11 @@ import Foundation
  오리지널 사운드 리스트
  */
 let originalSounds = [
-    OriginalSound(name: "물방울", filter: .waterDrop, imageName: "WaterDrop", defaultColor: "DCE8F5"),
-    
+    OriginalSound(name: "물소리", filter: .waterDrop, category: .waterDrop, defaultColor: "DCE8F5"),
     // TODO: 이미지 완성되면 imageName, defaultColor 수정 해야함
-    OriginalSound(name: "싱잉볼", filter: .singingBowl, imageName: "Wind", defaultColor: "DCE8F5"),
-    
+    OriginalSound(name: "싱잉볼", filter: .singingBowl, category: .singingBowl, defaultColor: "DCE8F5"),
     // TODO: 이미지 완성되면 imageName, defaultColor 수정 해야함
-    OriginalSound(name: "새소리", filter: .bird, imageName: "Forest", defaultColor: "DCE8F5")
+    OriginalSound(name: "새소리", filter: .bird, category: .bird, defaultColor: "DCE8F5")
 ]
 
 /**
