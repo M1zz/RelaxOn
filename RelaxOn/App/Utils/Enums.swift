755d3be104080e2e47f1e5c206fa59fadd525dbe
hamsik22 <payback9488@gmail.com>
2023-05-24 23:25:28 +0900

    🐛[Fix]: Fix Merge error
    	* 머지 이후 뜨는 에러코드를 수정

--- RelaxOn/App/Utils/Enums.swift
+++ RelaxOn/App/Utils/Enums.swift
@@ -52,4 +52,8 @@ enum OnboardInfo {
         case lamp = "수면에 도움되는 소리를 검색하고 하나하나 들어보느라 오래 걸린 적 있나요?"
         case musicplayer = "사운드를 선택하고 원하는 시간만큼 타이머를 설정한 후 숙면을 취해보세요."
     }
+    // 튜토리얼 관련
+    enum Tutorial: String {
+        case tutorialImage = "tutorialGesture"
+    }
 }
