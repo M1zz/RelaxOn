d793abccc6e0b21e09cbd6ea92b3f93bfb2244d1
doyeonjeong <debby_@kakao.com>
2023-05-26 19:03:00 +0900

    🎨[Design]: SoundDetailView UI 수정
    - 다크모드, 라이트모드 적용

--- RelaxOn/App/Views/Home/SoundDetailView.swift
+++ RelaxOn/App/Views/Home/SoundDetailView.swift
@@ -17,70 +17,83 @@ struct SoundDetailView: View {
     @State var isShowingSheet: Bool = false
     @State var originalSound: OriginalSound
     @EnvironmentObject var viewModel: CustomSoundViewModel
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        VStack {
+        ZStack {
+            Color(.DefaultBackground)
+                .ignoresSafeArea()
+            
             VStack {
-                Text("당신이 원하는 소리를 찾아가보세요")
-                    .foregroundColor(.black)
-                    .font(.title2)
-                    .padding(8)
-                Text("자유롭게 이동하며 실험해보세요")
-                    .foregroundColor(.black)
-                    .font(.title3)
-            }
-            ZStack {
-                
-                Circle()
-                    .frame(width: 300)
-                    .foregroundColor(Color("SystemGrey1"))
-                
-                // TODO: 슬라이더 총 4개 필요
-                // TODO: 각 슬라이더의 기능별 이미지 추가
-                // TODO: 예시 - Slider(value: $viewModel.speed, in: 0.0...1.0, step: 0.1)
-                CircleSlider(width: 300)
-                CircleSlider(width: 210)
-                CircleSlider(width: 120)
-                
-                // TODO: 컬러 설정 변경 필요
-                Circle()
-                    .stroke(Color("SystemGrey2"), style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
-                    .frame(width: 80)
-                
-                // TODO: Figma에 있는 이미지와 다름 -> Figma와 동일한 이미지로 Asset 추가하고 변경 필요
-                Image(systemName: "headphones")
-                
-            }
-            .padding(24)
-        }
-        
-        .navigationBarTitle(originalSound.name, displayMode: .inline)
-        .font(.system(size: 24, weight: .bold))
-        
-        .toolbar {
-            Button {
-                print("tapped done button")
-                isShowingSheet.toggle()
-            } label: {
-                Text("Done")
-                    .foregroundColor(.black)
-                    .bold()
-                    .font(.system(size: 20))
+                VStack {
+                    Text("당신이 원하는 소리를 찾아가보세요")
+                        .foregroundColor(Color(.Text))
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(8)
+                    Text("자유롭게 이동하며 실험해보세요")
+                        .foregroundColor(Color(.Text))
+                        .font(.system(size: 18, weight: .regular))
+                }
+
+                ZStack {
+                    
+                    Circle()
+                        .frame(width: 300)
+                        .foregroundColor(Color("SystemGrey1"))
+                    
+                    // TODO: 슬라이더 총 4개 필요
+                    // TODO: 각 슬라이더의 기능별 이미지 추가
+                    // TODO: 예시 - Slider(value: $viewModel.speed, in: 0.0...1.0, step: 0.1)
+                    CircleSlider(width: 300)
+                    CircleSlider(width: 210)
+                    CircleSlider(width: 120)
+                    
+                    // TODO: 컬러 설정 변경 필요
+                    Circle()
+                        .stroke(Color("SystemGrey2"), style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                        .frame(width: 80)
+                    
+                    // TODO: Figma에 있는 이미지와 다름 -> Figma와 동일한 이미지로 Asset 추가하고 변경 필요
+                    Image(systemName: "headphones")
+                    
+                }
+                .padding(24)
             }
             
-            .fullScreenCover(isPresented: $isShowingSheet, onDismiss: {
-                // TODO: Modal이 Dismiss됐을 때 소리 다시 재생하는 기능
-            }) {
-                SoundSaveView(originalSound: originalSound, audioVariation: AudioVariation(volume: viewModel.volume, pitch: viewModel.pitch, speed: viewModel.speed))
+            .navigationBarTitle(originalSound.name, displayMode: .inline)
+            .font(.system(size: 24, weight: .bold))
+            
+            .toolbar {
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .foregroundColor(Color(.ChevronBack))
+                            .frame(width: 30, height: 30)
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isShowingSheet.toggle()
+                    } label: {
+                        Text("다음")
+                            .foregroundColor(Color(.PrimaryPurple))
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                }
             }
-        }
-        
-        // MARK: - Life Cycle
-        .onAppear() {
-            viewModel.playSound(originSound: originalSound)
-        }
-        .onDisappear() {
-            viewModel.stopSound()
+            .navigationBarBackButtonHidden(true)
+            
+            // MARK: - Life Cycle
+            .onAppear() {
+                viewModel.playSound(originSound: originalSound)
+            }
+            .onDisappear() {
+                viewModel.stopSound()
+            }
         }
     }
 }
