89849f962c25eaefb27fedaa9c0d6f9fa8608c18
doyeonjeong <debby_@kakao.com>
2023-05-29 16:39:06 +0900

    🗑️[Del]: MixedSound 객체

--- RelaxOn/App/Models/CustomSound.swift
+++ RelaxOn/App/Models/CustomSound.swift
@@ -7,47 +7,85 @@
 
 import Foundation
 
-// TODO: CustomSound 객체로 교체 필요
-/**
- 사용자가 커스텀한 사운드 정보를 저장하고 불러오기 위한 구조체
- */
-struct MixedSound: Identifiable, Codable {
-    let id: UUID             // 생성된 MixedSound의 고유 아이디
-    var name: String         // 생성된 MixedSound의 name
-    var volume: Float        // 저장할 볼륨 값
-    var imageName: String    // 저장할 이미지
-    var audioFileURL: String // 저장된 오디오 파일의 URL
-    
-    init(name: String, volume: Float = 0.5, imageName: String = "placeholderImage", audioFileURL: String = "") {
-        self.id = UUID()
-        self.name = name
-        self.volume = volume
-        self.imageName = imageName
-        self.audioFileURL = audioFileURL
-    }
-}
-
 /**
  사용자가 커스텀한 사운드 정보를 저장하고 불러오기 위한 구조체
  */
 struct CustomSound: Identifiable, Codable, Equatable {
     
     let id: UUID
-    var fileName: String
+    var title: String
     var category: SoundCategory
     var audioVariation: AudioVariation
-    var audioFilter: AudioFilter
+    var filter: AudioFilter
+    var color: String
     
-    init(fileName: String, category: SoundCategory, audioVariation: AudioVariation, audioFilter: AudioFilter) {
+    init(fileName: String, category: SoundCategory, audioVariation: AudioVariation, audioFilter: AudioFilter, color: String = "") {
         self.id = UUID()
-        self.fileName = fileName
+        self.title = fileName
         self.category = category
         self.audioVariation = audioVariation
-        self.audioFilter = audioFilter
+        self.filter = audioFilter
+        self.color = (color == "") ? category.defaultColor : color
     }
     
-    static func ==(lhs: CustomSound, rhs: CustomSound) -> Bool {
+    static func == (lhs: CustomSound, rhs: CustomSound) -> Bool {
         return lhs.id == rhs.id
     }
+}
+
+// TODO: Enum 파일에 옮길거임
+/// 오디오 필터 관리를 위한 열거형
+enum AudioFilter: String, Codable {
+    case WaterDrop, Basement, Cave, Pipe, Sink
+    case SingingBowl, Focus, Training, Emptiness, Vibration
+    case Bird, Owl, Woodpecker, Forest
+}
+
+/// 원본 사운드 유형 관리를 위한 열거형
+enum SoundCategory: String, Codable {
+    case waterDrop, singingBowl, bird
     
+    var displayName: String {
+        switch self {
+        case .waterDrop:
+            return "물방울"
+        case .singingBowl:
+            return "싱잉볼"
+        case .bird:
+            return "새소리"
+        }
+    }
+    
+    var fileName: String {
+        switch self {
+        case .waterDrop:
+            return "WaterDrop"
+        case .singingBowl:
+            return "SingingBowl"
+        case .bird:
+            return "Bird"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .waterDrop:
+            return "WaterDrop"
+        case .singingBowl:
+            return "Wind" // TODO: 이미지 업데이트 후 변경 필요
+        case .bird:
+            return "Forest" // TODO: 이미지 업데이트 후 변경 필요
+        }
+    }
+    
+    var defaultColor: String {
+        switch self {
+        case .waterDrop:
+            return "DCE8F5"
+        case .singingBowl:
+            return "DCE8F5" // TODO: 이미지 업데이트 후 변경 필요
+        case .bird:
+            return "DCE8F5" // TODO: 이미지 업데이트 후 변경 필요
+        }
+    }
 }
