6e6d1a7f69ea0d4ca5ddcdb504029a29d55c7043
doyeonjeong <debby_@kakao.com>
2023-05-30 22:01:50 +0900

    🐛[Fix]: 일부 Enum 수정

--- RelaxOn/App/Utils/Enums.swift
+++ RelaxOn/App/Utils/Enums.swift
@@ -50,7 +50,7 @@ enum PlayerButton: String {
 enum FeatureIcon: String {
     case filter = "filter"
     case volume = "volume"
-    case fitch = "pitch"
+    case pitch = "pitch"
     case interval = "interval"
     // SoundDetailView에서만 쓰이는 이미지
     case headset = "headset"
@@ -60,21 +60,21 @@ enum FeatureIcon: String {
 enum OnboardInfo {
     // 아이콘 파일이름
     enum IconName: String {
-        case equalizerbutton = "equalizerbutton"
-        case headphone = "headphone"
-        case lamp = "lamp"
-        case musicplayer = "musicplayer"
+        case equalizerbutton = "Equalizer"
+        case headphone = "Headphone"
+        case lamp = "Lamp"
+        case musicplayer = "MusicPlayer"
     }
     // 설명 문구
     enum IconText: String {
-        case equlizerbutton = "나만의 소리를 찾기 위해 소리를 커스텀해보세요."
+        case equlizerbutton = "나만의 소리를 찾기 위해\n소리를 커스텀해보세요."
         case headphone = "지금 바로 나만의 소리를 만들어 보세요!"
-        case lamp = "수면에 도움되는 소리를 검색하고 하나하나 들어보느라 오래 걸린 적 있나요?"
-        case musicplayer = "사운드를 선택하고 원하는 시간만큼 타이머를 설정한 후 숙면을 취해보세요."
+        case lamp = "수면에 도움되는 소리를 검색하고\n하나하나 들어보느라 오래 걸린 적 있나요?"
+        case musicplayer = "사운드를 선택하고 원하는 시간만큼\n타이머를 설정한 후 숙면을 취해보세요."
     }
     // 튜토리얼 관련
     enum Tutorial: String {
-        case tutorialImage = "tutorialGesture"
+        case image = "TutorialGesture"
     }
 }
 
