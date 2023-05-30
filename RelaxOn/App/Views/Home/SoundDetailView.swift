7ff6cf294a5ca30a746e5cfdf451cbe6003c6b88
doyeonjeong <debby_@kakao.com>
2023-05-30 22:20:16 +0900

    🐛[Fix]: isTutorial 반영 - SoundDetailView

--- RelaxOn/App/Views/Home/SoundDetailView.swift
+++ RelaxOn/App/Views/Home/SoundDetailView.swift
@@ -14,6 +14,7 @@ import AVFoundation
 struct SoundDetailView: View {
     
     // MARK: - Properties
+    let isTutorial: Bool
     @State var isShowingSheet: Bool = false
     @State var originalSound: OriginalSound
     @EnvironmentObject var viewModel: CustomSoundViewModel
@@ -36,10 +37,24 @@ struct SoundDetailView: View {
                 }
                 ZStack {
                     backgroundCircle()
-                    CircularSlider(width: circleWidth[0], imageName: featureIcon[0], gestureType: true) { angle in }
-                    CircularSlider(width: circleWidth[1], imageName: featureIcon[1], gestureType: true) { angle in }
-                    CircularSlider(width: circleWidth[2], imageName: featureIcon[2], gestureType: true) { angle in }
-                    CircularSlider(width: circleWidth[3], imageName: featureIcon[3], gestureType: false) { angle in }
+                    CircularSlider(width: circleWidth[0], imageName: featureIcon[0], gestureType: true, range: viewModel.intervalRange) { angle in
+                        print("간격 : \(angle)")
+                        viewModel.speed = Float(1.0 - abs(angle * 0.00556))
+                        
+                    }
+                    CircularSlider(width: circleWidth[1], imageName: featureIcon[1], gestureType: true, range: viewModel.volumeRange) { angle in
+                        print("볼륨 : \(1.0 - abs(angle * 0.00556))")
+                        viewModel.volume = Float((1.0 - abs(angle * 0.00556)))
+                    }
+                    CircularSlider(width: circleWidth[2], imageName: featureIcon[2], gestureType: true, range: viewModel.pitchRange) { angle in
+                        print("높낮이 : \(angle * 13.4)")
+                        viewModel.pitch = Float(angle * 13.4)
+                    }
+                    // TODO: 필터기능 구현
+                    CircularSlider(width: circleWidth[3], imageName: featureIcon[3], gestureType: false, range: viewModel.filterRange) { angle in
+                        print("필터 : \(angle)")
+                        
+                    }
                     
                 }
                 .padding(24)
@@ -72,20 +87,58 @@ struct SoundDetailView: View {
             }
             .navigationBarBackButtonHidden(true)
             
+            .fullScreenCover(isPresented: $isShowingSheet) {
+                SoundSaveView(originalSound: originalSound, audioVariation: AudioVariation(volume: viewModel.volume, pitch: viewModel.pitch, speed: viewModel.speed), audioFilter: originalSound.filter, backgroundColor: originalSound.color)
+            }
+            
             // MARK: - Life Cycle
             .onAppear() {
-                if !viewModel.isPlaying {
+                if !isTutorial {
+                    viewModel.isPlaying = true
                     viewModel.playSound(originSound: originalSound)
                 }
             }
-            .onDisappear() {
-                if viewModel.isPlaying {
-                    viewModel.stopSound()
-                }
+            .onDisappear {
+                presentationMode.wrappedValue.dismiss()
+                viewModel.stopSound()
             }
-            .fullScreenCover(isPresented: $isShowingSheet) {
-                // TODO: 값을 넘겨주기 전에 변경된 값으로 AudioVariation 만들어서 보내야함
-                SoundSaveView(originalSound: originalSound, audioVariation: AudioVariation())
+        }
+    }
+    
+    // 배경으로 쓰이는 원 + 원형 라인 + 이동 포인트
+    @ViewBuilder
+    func backgroundCircle() -> some View {
+        
+        ZStack {
+            
+            Circle()
+                .fill(Color.relaxDimPurple)
+                .frame(width: 300)
+                .opacity(0.3)
+            
+            Circle()
+                .stroke(style: .init(lineWidth: 1))
+                .foregroundColor(.relaxDimPurple)
+                .frame(width: 80)
+            
+            Image(FeatureIcon.headset.rawValue)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 26)
+            
+            ForEach(0..<circleWidth.count, id: \.self) { index in
+                Circle()
+                    .stroke(style: .init(lineWidth: 1))
+                    .foregroundColor(.relaxDimPurple)
+                    .frame(width: circleWidth[index])
+            }
+            
+            ForEach(0..<pointAngle.count, id: \.self) { index in
+                Circle()
+                    .frame(width: 6)
+                    .foregroundColor(.purple)
+                    .offset(x: 300 / 2)
+                    .rotationEffect(.init(degrees: pointAngle[index]))
             }
         }
     }
@@ -93,6 +146,6 @@ struct SoundDetailView: View {
 
 struct SoundDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        SoundDetailView(originalSound: OriginalSound(name: "물방울", filter: .WaterDrop, category: .waterDrop))
+        SoundDetailView(isTutorial: true, originalSound: OriginalSound(name: "물방울", filter: .WaterDrop, category: .waterDrop))
     }
 }
