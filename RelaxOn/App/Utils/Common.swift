8e5c1b2f0f90e0c9fca8cb9534c771b490c9795d
doyeonjeong <debby_@kakao.com>
2023-05-30 22:02:08 +0900

    🐛[Fix]: Common 파일 일부 수정

--- RelaxOn/App/Utils/Common.swift
+++ RelaxOn/App/Utils/Common.swift
@@ -23,21 +23,11 @@ func getPathNSURL(forResource: String, musicExtension: MusicExtension) -> NSURL?
 }
 
 /**
- 온보딩화면 이미지 프로퍼티
- */
-struct OnboardItem {
-    var imageName: String
-    var description: String
-}
-var tutorialImage = "TutorialGesture"
-
-/**
  SoundDetailView의 프로퍼티
  */
 var circleWidth: [CGFloat] = [120, 210, 250, 300]
 var pointAngle: [Double] = [72, 144, 216, 288, 360]
 var featureIcon: [String] = [FeatureIcon.interval.rawValue,
                              FeatureIcon.volume.rawValue,
-                             FeatureIcon.fitch.rawValue,
+                             FeatureIcon.pitch.rawValue,
                              FeatureIcon.filter.rawValue]
-
