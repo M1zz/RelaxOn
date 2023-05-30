ffd2427fb1856ae1717ce050ee5c6dc0a5fb5bed
doyeonjeong <debby_@kakao.com>
2023-05-30 22:18:46 +0900

    ✨[Feat]: 최초 진입시 온보딩 반영

--- RelaxOn/App/Views/MainTabView.swift
+++ RelaxOn/App/Views/MainTabView.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
     @EnvironmentObject var appState: AppState
+    @State var isFirstVisit: Bool = UserDefaultsManager.shared.isFirstVisit
     
     var body: some View {
-        TabView(selection: $appState.selectedTab) {
-            ForEach(appState.tabItems) { item in
-                item.view
-                    .tabItem {
-                        Image(item.imageName.rawValue)
-                            .renderingMode(.template)
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                        Text(item.title.rawValue)
-                    }.tag(item.id)
+        if isFirstVisit {
+            OnboardingView(isFirstVisit: $isFirstVisit)
+                .transition(.slide)
+        } else {
+            TabView(selection: $appState.selectedTab) {
+                ForEach(appState.tabItems) { item in
+                    item.view
+                        .tabItem {
+                            Image(item.imageName.rawValue)
+                                .renderingMode(.template)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                            Text(item.title.rawValue)
+                        }.tag(item.id)
+                }
             }
+            .accentColor(Color(.PrimaryPurple))
+            .environmentObject(appState)
         }
-        .accentColor(Color(.PrimaryPurple))
-        .environmentObject(appState)
     }
 }
 
 struct CustomTabBar_Previews: PreviewProvider {
     static var previews: some View {
-        MainTabView()
+        MainTabView(isFirstVisit: true)
     }
 }
