84fcf84283ebecbd0461f1d30254d54e344d5b65
hamsik22 <payback9488@gmail.com>
2023-06-08 00:21:26 +0900

    ✏️[Comment]: 주석 추가

--- RelaxOn/App/Views/Components/Pre)CircularSlider.swift
+++ RelaxOn/App/Views/Components/Pre)CircularSlider.swift
@@ -4,11 +4,18 @@
 //
 //  Created by 황석현 on 2023/06/08.
 //
-
+/**
+ SoundDetailView의 사용되는 원형슬라이더 뷰 본문
+ */
 import SwiftUI
 
 struct Pre_CircularSlider: View {
+    // TODO: 슬라이더의 크기, 최대값을 입력받는다
+    // TODO: 슬라이더의 값을 측정,계산 한다
+    // TODO: 슬라이더의 값을 반환,저장 한다
     var body: some View {
+        // TODO: 슬라이더 UI를 구현한다
+        // TODO: 원형의 선과 사용자가 터치할 원(Image)를 구현한다
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
