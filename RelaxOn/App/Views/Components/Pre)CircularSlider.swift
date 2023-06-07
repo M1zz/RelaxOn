18769cc8c894ad5fcc2c1488b999dee7f67d0b69
hamsik22 <payback9488@gmail.com>
2023-06-08 00:14:54 +0900

    ➕[Add]: 새로운 Slider 구현을 위한 파일 추가
    	* 기존 코드를 보존하고 새로 작업하여 추후 적용 후
    	  기존 파일 삭제 후 수정 예정

---
+++ RelaxOn/App/Views/Components/Pre)CircularSlider.swift
@@ -0,0 +1,20 @@
+//
+//  Pre)CircularSlider.swift
+//  RelaxOn
+//
+//  Created by 황석현 on 2023/06/08.
+//
+
+import SwiftUI
+
+struct Pre_CircularSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pre_CircularSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        Pre_CircularSlider()
+    }
+}
