f1c3dd1cae48f8f8a61f5b44038dbb90e7f7acc0
DOYEON <108422901+doyeonjeong@users.noreply.github.com>
2023-06-14 15:51:56 +0900

    🐛[Fix]: Play 되지 않던 오류 해결 (#434)
    * 🐛[Fix]: Play 되지 않던 오류 해결
    
    * 🗑️ [Del]: 불필요한 코드 삭제
    
    * 🗑️ [Del]: 코드 정리
    
    * 🗑️ [Del]: 사용하지 않는 파일 삭제
    
    * ✏️[Comment]: CustomSoundViewModel 프로퍼티 주석 추가
    
    * 🐛[Fix]: isPlaying 로직 수정
    
    * 🐛[Fix]: play(with:)수정

--- RelaxOn/App/ViewModel/CustomSoundViewModel.swift
+++ RelaxOn/App/ViewModel/CustomSoundViewModel.swift
@@ -14,69 +14,88 @@ import SwiftUI
 final class CustomSoundViewModel: ObservableObject {
     
     // MARK: - Properties
+    /// 필터가 변경되었을 때 호출될 클로저
     var isFilterChanged: (() -> Void)?
     
     private var audioEngineManager = AudioEngineManager.shared
     private var fileManager = UserFileManager.shared
     private var userDefaults = UserDefaultsManager.shared
+    
+    /// 각 CustomSound 객체에 대응하는 인덱스를 저장하는 사전
     private(set) var customSoundsDictionary: [Int: CustomSound] = [:]
     
     let intervalRange: [Float] = stride(from: 0.5, through: 5.0, by: 0.1).map { Float($0) }
     let pitchRange: [Float] = stride(from: -50.0, through: 50.0, by: 10.0).map { Float($0) }
     let volumeRange: [Float] = stride(from: 0.0, through: 1.0, by: 0.01).map { Float($0) }
+    
+    // TODO: 필요없음. 없으면 안되는건지 재확인 후 삭제
     let filterRange: [Float] = stride(from: -50.0, through: 50.0, by: 1.0).map { Float($0) }
     
+    /// 각 오디오 필터에 대한 서브 필터를 저장하는 딕셔너리
     let filterDictionary: [AudioFilter: [AudioFilter]] = [
         .WaterDrop: [.WaterDrop, .Basement, .Cave, .Pipe, .Sink],
         .SingingBowl: [.SingingBowl, .Focus, .Training, .Vibration],
         .Bird: [.Bird, .Owl, .Woodpecker, .Forest, .Cuckoo],
     ]
   
+    /// 현재 재생되는 소리
+    /// - 기본값 : OriginalSound(name: "물방울", filter: .WaterDrop, category: .waterDrop)
     @Published var sound: Playable = OriginalSound(name: "물방울", filter: .WaterDrop, category: .waterDrop)
+    
     @Published var color = ""
     @Published var searchText = ""
     @Published var isPlaying = false
+    
+    /// 현재 선택된 소리의 인덱스
     @Published var currentSoundIndex: Int = 0
     
+    /// 가장 마지막에 재생된 Sound 정보
     @Published var lastSound: CustomSound {
         didSet {
             userDefaults.lastPlayedSound = lastSound
         }
     }
     
+    /// 현재 선택된 음원 저장
     @Published var selectedSound: CustomSound? = nil {
         didSet {
             if let selected = selectedSound, let index = customSoundsDictionary.first(where: { $0.value == selected })?.key {
+                // 선택된 음원의 인덱스를 저장
                 currentSoundIndex = index
             }
         }
     }
     
+    /// UserDefaults에 저장된 커스텀 음원 배열
     @Published var customSounds: [CustomSound] = [] {
         didSet {
-            UserDefaultsManager.shared.customSounds = customSounds
-            customSoundsDictionary = Dictionary(uniqueKeysWithValues: zip(customSounds.indices, customSounds))
+            UserDefaultsManager.shared.customSounds = customSounds // 변경된 커스텀 음원 배열을 UserDefaults에 저장
+            customSoundsDictionary = Dictionary(uniqueKeysWithValues: zip(customSounds.indices, customSounds)) // 커스텀 음원 배열을 딕셔너리로 변환하여 저장 [배열 인덱스: 배열의 요소]
         }
     }
     
+    /// sound의 재생 간격 저장
     @Published var interval = Float() {
         didSet {
             audioEngineManager.audioVariation.interval = interval
         }
     }
     
+    /// sound의 피치 저장
     @Published var pitch = Float() {
         didSet {
             audioEngineManager.audioVariation.pitch = pitch
         }
     }
     
+    /// sound의 볼륨 저장
     @Published var volume = Float() {
         didSet {
             audioEngineManager.audioVariation.volume = volume
         }
     }
     
+    /// sound의 filter 저장
     @Published var filter: AudioFilter {
         didSet {
             selectedSound?.filter = filter
@@ -84,10 +103,12 @@ final class CustomSoundViewModel: ObservableObject {
         }
     }
     
+    /// 현재 재생 상태에 따른 버튼 이미지 Asset 이름
     var playPauseStatusImage: String {
         isPlaying ? PlayerButton.pause.rawValue : PlayerButton.play.rawValue
     }
     
+    /// 검색 텍스트에 따라 필터링된 음원 배열
     var filteredSounds: [CustomSound] {
         if searchText.isEmpty {
             return customSounds
@@ -96,6 +117,7 @@ final class CustomSoundViewModel: ObservableObject {
         }
     }
     
+    /// 클래스 초기화 시 선택된 음원과 필터를 설정하고 재생된 음원을 불러옴
     init(customSound: CustomSound? = nil, filter: AudioFilter = .none) {
         self.selectedSound = customSound
         self.filter = filter
