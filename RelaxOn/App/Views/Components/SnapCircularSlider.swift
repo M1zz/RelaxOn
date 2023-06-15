9e5a2b613857142fde3900c9694bd27cbf1c4575
hamsik22 <payback9488@gmail.com>
2023-06-15 19:20:30 +0900

    ➕[Add]: Add File-SnapCircularSlider

---
+++ RelaxOn/App/Views/Components/SnapCircularSlider.swift
@@ -0,0 +1,20 @@
+//
+//  SnapCircularSlider.swift
+//  RelaxOn
+//
+//  Created by 황석현 on 2023/06/15.
+//
+
+import SwiftUI
+
+struct SnapCircularSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnapCircularSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        SnapCircularSlider()
+    }
+}
