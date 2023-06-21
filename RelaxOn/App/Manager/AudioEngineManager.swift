da4f44dcaef32f864e090ec0b7fa33e82dc8eb19
doyeonjeong <debby_@kakao.com>
2023-06-21 16:12:02 +0900

    🐛[Fix]: interval 최종 수정

--- RelaxOn/App/Manager/AudioEngineManager.swift
+++ RelaxOn/App/Manager/AudioEngineManager.swift
@@ -19,6 +19,8 @@ final class AudioEngineManager: ObservableObject {
     private var audioFile: AVAudioFile?
     private var audioBuffer: AVAudioPCMBuffer?
     private var scheduleCompletionHandler: (() -> Void)?
+    private var intervalCancellable: AnyCancellable?
+    private var timerSubscription: Cancellable?
     
     @Published var interval: Double = 1.0 {
         didSet {
@@ -49,6 +51,7 @@ final class AudioEngineManager: ObservableObject {
     private init() {
         setupAudioSession()
         setupEngine()
+        setupSubscriptions()
     }
 }
 
@@ -76,6 +79,15 @@ extension AudioEngineManager {
             engine.connect(pitchEffect, to: volumeEffect, format: audioFile.processingFormat)
             engine.connect(volumeEffect, to: engine.mainMixerNode, format: audioFile.processingFormat)
         }
+    }
+    
+    private func setupSubscriptions() {
+        intervalCancellable = $interval
+            .removeDuplicates()
+            .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                self?.scheduleNextBuffer()
+            }
     }
     
 }
@@ -146,32 +158,38 @@ extension AudioEngineManager {
             return
         }
         
-        let scheduleTime = AVAudioTime(hostTime: mach_absolute_time())
-        player.scheduleBuffer(buffer, at: scheduleTime) { [weak self] in
-            guard let self = self else { return }
-            
-            self.scheduleCompletionHandler?()
-            self.scheduleCompletionHandler = nil
-            
-            DispatchQueue.main.async {
-                if self.audioBuffer != nil {
-                    self.scheduleNextBuffer()
+        // 취소 가능한 타이머를 만듭니다.
+        timerSubscription?.cancel()
+        timerSubscription = Timer.publish(every: interval, on: RunLoop.main, in: .common)
+            .autoconnect()
+            .first()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                self.player.pause()
+                
+                let scheduleTime = AVAudioTime(hostTime: mach_absolute_time())
+                self.player.scheduleBuffer(buffer, at: scheduleTime) { [weak self] in
+                    self?.scheduleCompletionHandler?()
+                    self?.scheduleCompletionHandler = nil
+                    
+                    DispatchQueue.main.async {
+                        if self?.audioBuffer != nil {
+                            self?.scheduleNextBuffer()
+                        }
+                    }
+                }
+                
+                if !self.engine.isRunning {
+                    do {
+                        try self.engine.start()
+                    } catch {
+                        print("Unable to start engine: \(error.localizedDescription)")
+                    }
+                }
+                
+                if !self.player.isPlaying {
+                    self.player.play()
                 }
             }
-        }
-        
-        if !engine.isRunning {
-            do {
-                try engine.start()
-            } catch {
-                print("Unable to start engine: \(error.localizedDescription)")
-            }
-        }
-        
-        if !player.isPlaying {
-            player.play()
-        }
     }
-
-    
 }
